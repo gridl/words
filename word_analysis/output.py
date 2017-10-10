d8d2ee3d927ea9a54aaa52a7a3e3c0126f72fa79
nicolas <nikolay.kurochkin@fogstream.ru>
2017-10-10 18:26:46 +1000

    to csv import

--- word_analysis/output.py
+++ word_analysis/output.py
@@ -11,15 +11,16 @@ def to_json(words):
     data = json.dumps(words)
     with open('words.json', 'w') as f:
         f.write(data)
+    print('Imported into words.json')
 
 
 def to_csv(words):
-    with open('words.csv', 'wb') as out:
-        csv_out = csv.writer(out)
-        # csv_out.writerow(['name', 'num'])
-        # for word in words:
-        #     csv_out.writerow(word)
-        csv_out.writerows(words)
+    with open('words.csv', 'w', newline='') as csvfile:
+        csv_out = csv.writer(csvfile, delimiter=' ',
+                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for word in words:
+            csv_out.writerow(word)
+    print('Imported into words.csv')
 
 
 def make_ouput(words, output_method):
